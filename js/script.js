fe090bae52db7e49863f188a01c1bff967ae79cb
Luisa Logozzo <luisa.logozzo@gmail.com>
2020-02-12 14:52:51 +0100

    up

--- js/script.js
+++ js/script.js
@@ -1,28 +1,26 @@
-// Creare un layout base con una searchbar (una input e un button) in cui possiamo scrivere completamente o parzialmente il nome di un film. Possiamo, cliccando il  bottone, cercare sull’API tutti i film che contengono ciò che ha scritto l’utente.
-// Vogliamo dopo la risposta dell’API visualizzare a schermo i seguenti valori per ogni film trovato:
-// Titolo
-// Titolo Originale
-// Lingua
-// Voto
-
 $(document).ready(function () {
+  var UrlMovie = 'https://api.themoviedb.org/3/search/movie';
+  var ContainerMovie = '.container-film > ul';
+  var UrlSeries = 'https://api.themoviedb.org/3/search/tv';
+  var ContainerSeries = '.container-series > ul';
+
+
   $( "#insert-film-name").keyup(function() {
    if (event.which == 13) {
      var InsertedFilm = $('#insert-film-name').val();
-     printFilms(InsertedFilm);
-     // printSeries(InsertedFilm);
+     printMovieTv(InsertedFilm, UrlMovie, ContainerMovie);
+     printMovieTv(InsertedFilm, UrlSeries, ContainerSeries);
    }
   });
 
   $(document).on('click', '#search', function() {
     var InsertedFilm = $('#insert-film-name').val();
-    printFilms(InsertedFilm);
-    // printSeries(InsertedFilm);
+    printMovieTv(InsertedFilm, UrlMovie, ContainerMovie);
+    printMovieTv(InsertedFilm, UrlSeries, ContainerSeries);
   });
 
 
   $('.retrocard').hide();
-
   $(document).on('mouseenter','.filmseries .poster', function () {
       $( this ).removeClass('z-index-1');
       $( this ).hide();
@@ -31,15 +29,13 @@ $(document).ready(function () {
       $( this ).find('.poster').show();
   });
 
-
-
 });
 
-function printFilms(InsertedFilm) {
+function printMovieTv(InsertedFilm, url, container) {
   $('#insert-film-name').val('');
-  $('.container-film > ul').html('');
+  $(container).html('');
   $.ajax({
-    url: 'https://api.themoviedb.org/3/search/movie',
+    url: url,
     method: 'GET',
     data: {
     api_key: '25046906a5edc120a00e8cdb72843203',
@@ -51,20 +47,28 @@ function printFilms(InsertedFilm) {
       var template = Handlebars.compile(source);
       var filmVote;
       var filmVoteTo5;
+      var movieId;
+      var ThisResults;
       for (var i = 0; i < risposta.results.length; i++) {
-        filmVote = parseInt(risposta.results[i].vote_average) / 2;
-        filmVoteTo5 = Math.round(filmVote);
+        ThisResults = risposta.results[i];
+        movieId = ThisResults.id;
+        console.log(movieId);
+        filmVote = parseInt(ThisResults.vote_average) / 2;
+        filmVoteTo5 = Math.ceil(filmVote);
         var context = {
-          poster_path: risposta.results[i].poster_path,
-          filmseries: 'film',
-          title: risposta.results[i].title,
-          original_title: risposta.results[i].original_title,
-          language: risposta.results[i].original_language,
+          poster_path: ThisResults.poster_path,
+          // filmseries: 'film',
+          title: ThisResults.title,
+          original_title: ThisResults.original_title,
+          language: ThisResults.original_language,
+          name: ThisResults.name,
+          original_name: ThisResults.original_name,
           vote_average: printStars(filmVoteTo5),
-          overview: risposta.results[i].overview,
+          overview: ThisResults.overview,
+          cast: SearchActors(movieId),
          };
          var html = template(context);
-         $('.container-film ul').append(html);
+         $(container).append(html);
       }
     },
     error: function (request, state, errors) {
@@ -72,46 +76,6 @@ function printFilms(InsertedFilm) {
     }
   });
 }
-function printSeries(InsertedFilm) {
-  $('#insert-film-name').val('');
-  $('.container-series').html('');
-  $.ajax({
-    url: 'https://api.themoviedb.org/3/search/tv',
-    method: 'GET',
-    data: {
-    api_key: '25046906a5edc120a00e8cdb72843203',
-    query: InsertedFilm,
-    language: 'it-IT',
-    },
-    success: function (risposta) {
-      var source = document.getElementById("films-template").innerHTML;
-      var template = Handlebars.compile(source);
-      var filmVote;
-      var filmVoteTo5;
-      for (var i = 0; i < risposta.results.length; i++) {
-        filmVote = parseInt(risposta.results[i].vote_average) / 2;
-        filmVoteTo5 = Math.round(filmVote);
-        var context = {
-          poster_path: risposta.results[i].poster_path,
-          filmseries: 'series',
-          name: risposta.results[i].name,
-          original_name: risposta.results[i].original_name,
-          language: risposta.results[i].original_language,
-          vote_average: printStars(filmVoteTo5),
-         };
-         var html = template(context);
-         $('.container-series ul').append(html);
-      }
-    },
-    error: function (request, state, errors) {
-      console.log(errors);
-    }
-  });
-}
-
-
-
-
 function printStars (vote) {
   var star = '';
 
@@ -123,4 +87,25 @@ for (var i = 0; i < 5; i++) {
   }
 }
 return star;
+}
+
+function SearchActors(movieId) {
+  var Cast = '';
+  $.ajax({
+    url: 'https://api.themoviedb.org/3/movie/' + movieId + '/credits',
+    method: 'GET',
+    data: {
+    api_key: '25046906a5edc120a00e8cdb72843203',
+    },
+    success: function (risposta) {
+      for (var i = 0; i < risposta.cast.length; i++) {
+      Cast = risposta.cast[i].name;
+      console.log(Cast);
+    }
+    },
+    error: function (errors) {
+      console.log(errors);
+    }
+  });
+ setTimeout(function(){ return Cast; }, 500);
 }
